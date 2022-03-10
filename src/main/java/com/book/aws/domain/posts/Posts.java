bbdedf51a03bd3ff92213ba264bb6655d1973603
LEEINHEE <lee1675012>
2022-03-11 01:45:15 +0900

    Posts Entity 작성 Posts Repository 작성

---
+++ src/main/java/com/book/aws/domain/posts/Posts.java
@@ -0,0 +1,30 @@
+package com.book.aws.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_idx")
+    private Long id;
+    @Column(name = "title", nullable = false, length = 500)
+    private String title;
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+    @Column(name = "author")
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
--- src/test/java/com/book/aws/web/controller/HelloControllerTest.java
+++ src/test/java/com/book/aws/web/controller/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package com.book.aws.web.controller;
 
-import com.book.aws.web.controller.HelloController;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.regex.Matcher;
 
 import static org.hamcrest.Matchers.is;
 
