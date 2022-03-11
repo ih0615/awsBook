cf38ede4c117c967c5f692c1294e984bf6d9fa46
LEEINHEE <lee1675012>
2022-03-12 00:30:14 +0900

    JPA Auditing 적용

--- src/main/java/com/book/aws/AwsApplication.java
+++ src/main/java/com/book/aws/AwsApplication.java
@@ -2,7 +2,9 @@ package com.book.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AwsApplication {
     public static void main(String[] args) {
