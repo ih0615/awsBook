cd6db6925754abde51aa723353c8d9388677705f
LEEINHEE <lee1675012>
2022-03-12 03:00:09 +0900

    게시글 등록, 수정 작성

--- src/main/java/com/book/aws/service/posts/PostsService.java
+++ src/main/java/com/book/aws/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.book.aws.service.posts;
 
 import com.book.aws.domain.posts.Posts;
 import com.book.aws.domain.posts.PostsRepository;
+import com.book.aws.web.dto.PostsListResponseDto;
 import com.book.aws.web.dto.PostsResponseDto;
 import com.book.aws.web.dto.PostsSaveRequestDto;
 import com.book.aws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -30,5 +34,13 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findALLDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
