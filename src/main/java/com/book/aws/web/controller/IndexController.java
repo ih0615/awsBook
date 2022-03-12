125971521d3497a3964c14b0acb40459aa88f7fa
LEEINHEE <lee1675012>
2022-03-13 01:19:49 +0900

    구글 로그인 구현

--- src/main/java/com/book/aws/web/controller/IndexController.java
+++ src/main/java/com/book/aws/web/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.book.aws.web.controller;
 
+import com.book.aws.config.auth.dto.SessionUser;
 import com.book.aws.service.posts.PostsService;
 import com.book.aws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,16 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
