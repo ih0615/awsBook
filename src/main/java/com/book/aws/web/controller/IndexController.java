6b32c9745cab359078cb94444727ed081ca6141e
LEEINHEE <lee1675012>
2022-03-12 23:38:43 +0900

    게시글 수정 삭제 작성

--- src/main/java/com/book/aws/web/controller/IndexController.java
+++ src/main/java/com/book/aws/web/controller/IndexController.java
@@ -1,10 +1,13 @@
 package com.book.aws.web.controller;
 
 import com.book.aws.service.posts.PostsService;
+import com.book.aws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,5 +23,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto postsResponseDto = postsService.findById(id);
+        model.addAttribute("post", postsResponseDto);
+        return "posts-update";
     }
 }
